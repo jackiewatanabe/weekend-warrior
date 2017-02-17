a7fe29dcc75bd0f779d1aaeab5ca5aa061ae7230
Jackie Watanabe <sushiigirl@gmail.com>
2017-02-17 11:15:24 -0800

    finished Hello World! and updated ruby-version file to 2.4.0

--- 00-hello-world/hello_world_spec.rb
+++ 00-hello-world/hello_world_spec.rb
@@ -13,10 +13,12 @@ describe "Hello World" do
   end
 
   it "When given 'Bob' it should greet Bob!" do
+
     expect(hello_world 'Bob').must_equal 'Hello, Bob!'
   end
 
   it "When given an empty string it should greet the world!" do
+
     expect(hello_world '').must_equal 'Hello, World!'
   end
 end
