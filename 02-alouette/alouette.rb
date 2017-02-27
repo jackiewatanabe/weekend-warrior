f2df0d37c6c9471c15dbbbce84e07192c45052b5
Jackie Watanabe <sushiigirl@gmail.com>
2017-02-26 23:11:12 -0800

    adjusted array in class method 'ines for verse' to only include phrase and then when creating a new array, adds 'Et ' and exclamation point to the phrase.

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -8,14 +8,18 @@ class Alouette
 
 
   def self.lines_for_verse(verse_number)
-    @lyrics = [ "Et la tête!", "Et le bec!", "Et les yeux!", "Et le cou!", "Et les ailes!", "Et les pattes!", "Et la queue!", "Et le dos!" ]
+    @lyrics = [ "la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos" ]
     @lines = []
 
     until verse_number == -1
-      @lines << @lyrics[verse_number]
+      @lines << "Et #{@lyrics[verse_number]}!"
       verse_number -= 1
     end
     return @lines
+  end
+
+  def self.verse(verse_number)
+
   end
 
 end
